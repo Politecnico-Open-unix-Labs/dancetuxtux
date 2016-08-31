1184c893b9d89620d7a22e0150862411b4d47b45
Alessio Serraino <serrainoalessio@gmail.com>
2016-08-31 15:43:12 +0200

    Circular buffer bugfix

--- include/circular_buffer.h
+++ include/circular_buffer.h
@@ -4,21 +4,29 @@
 #include <stdint.h> // uint8_t, uint16_t
 #include <stddef.h> // NULL pointer
 
+#define USE_BINARY_BUFFER // define to enable binary buffer memory optimization (requires more computing power)
+
+#ifdef USE_BINARY_BUFFER
+#    define CIRCULAR_BUFFER_BUF_LEN(SAMPLES_NUM) ((SAMPLES_NUM + CHAR_BIT - 1)/CHAR_BIT) // Rounds up
+#else // not using binary buffer
+#    define CIRCULAR_BUFFER_BUF_LEN(SAMPLES_NUM) (SAMPLES_NUM) // One byte per sample
+#endif // USE_BINARY_BUFFER
+
+// Binary buffer (i.e. can store only 1s and 0s)
 struct _circular_buffer_t {
     uint8_t * data; // pointer to the beginning of the array
-    uint8_t len; // Lenght of the data
+    uint8_t len; // Lenght of the buffer (wich is not lenght of the data)
     uint8_t pos; // current position in the buffer
-
-    uint16_t sum; // Sum of all the data in the buffer
+    uint8_t sum; // Sum of all the data in the buffer
 };
 
 typedef struct _circular_buffer_t * circular_buffer_ptr_t;
 typedef const struct _circular_buffer_t * circular_buffer_const_ptr_t;
-typedef struct _circular_buffer_t circular_buffer_t[1];
+typedef struct _circular_buffer_t circular_buffer_t[1]; // util name
 
 void circular_buffer_init(circular_buffer_ptr_t buf, uint8_t * data, uint8_t len); // inits a buffer
 void circular_buffer_reset(circular_buffer_ptr_t buf);
 void circular_buffer_push(circular_buffer_ptr_t buf, uint8_t new_data); // pushes new data (popping the old one)
-uint16_t circular_buffer_sum(circular_buffer_const_ptr_t buf); // gets the sum of all the data into the buffer
+uint8_t circular_buffer_sum(circular_buffer_const_ptr_t buf); // gets the sum of all the data into the buffer
 
 #endif // CIRCULAR_BUFFER_H defined
--- src/circular_buffer.c
+++ src/circular_buffer.c
@@ -1,10 +1,47 @@
+/*
+    DanceTuxTux Board Project
+    Copyright (C) 2016  Serraino Alessio
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <string.h> // memset
+#include <limits.h>
 #include "circular_buffer.h"
+#include "cpu.h" // _BV definition
+
+#ifdef USE_BINARY_BUFFER
+#    define BIT_OF(address)  (address % CHAR_BIT)
+#    define BYTE_OF(address) (address / CHAR_BIT)
+#    define GET_BIT_AT(buffer, address) (!!( buffer[BYTE_OF(address)] & _BV(BIT_OF(address)) ))
+#    define SET_BIT_AT(buffer, address) (buffer[BYTE_OF(address)] |= _BV(BIT_OF(address)) )
+#    define CLR_BIT_AT(buffer, address) (buffer[BYTE_OF(address)] &= ~(_BV(BIT_OF(address))) )
+#    define WRITE_BIT_AT(buffer, address, val) (val)?SET_BIT_AT(buffer, address):CLR_BIT_AT(buffer, address)
+#else // USE_BINARY_BUFFER not defined
+#    define GET_BIT_AT(buffer, address)        (!!buffer[address]) // Gets binary value
+#    define WRITE_BIT_AT(buffer, address, val) (buffer[address] = !!val) // Sets binary value
+#endif // USE_BINARY_BUFFER
 
 void circular_buffer_reset(struct _circular_buffer_t * const buf) {
-    memset(buf->data, 0, (buf->len)*sizeof*(buf->data)); // fills the buffer with 0s
+    uint8_t i; // counter
+
+    memset(buf->data, 0, CIRCULAR_BUFFER_BUF_LEN(buf->len)*sizeof*(buf->data)); // fills the buffer with 0s
     buf->pos = 0; // This is arbitrary, but must be less than 'len'
     buf->sum = 0; // sum of all data
+
+    for (i = 0; i < buf->len; i++) // Fills with 50% 1 and 50% zero
+        circular_buffer_push(buf, i % 2);
 }
 
 void circular_buffer_init(struct _circular_buffer_t * const buf, uint8_t * data, const uint8_t len) {
@@ -14,14 +51,14 @@ void circular_buffer_init(struct _circular_buffer_t * const buf, uint8_t * data,
 }
 
 void circular_buffer_push(struct _circular_buffer_t * const buf, const uint8_t new_data) {
-    buf->sum -= buf->data[buf->pos]; // subtracts old data
-    buf->data[buf->pos] = new_data; // writes new in the buffer
-    buf->sum += buf->data[buf->pos]; // adds new data
+    buf->sum -= GET_BIT_AT(buf->data, buf->pos); // subtracts old data
+    WRITE_BIT_AT(buf->data, buf->pos, new_data); // writes new in the buffer
+    buf->sum += GET_BIT_AT(buf->data, buf->pos); // adds new data
     (buf->pos)++; // Increases the pointer
     if (buf->pos >= buf->len)
         buf->pos = 0; // Resets when pos reaches the end
 }
 
-uint16_t circular_buffer_sum(const struct _circular_buffer_t * const buf) {
+uint8_t circular_buffer_sum(const struct _circular_buffer_t * const buf) {
     return buf->sum;
 }
